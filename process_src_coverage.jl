f8c0e2b52bba2d0374d5032222cca9c063791d2b
Jason Eckstein <jekyllstein@gmail.com>
2019-11-02 05:10:16 -0600

    Skip legacy code for coverage

---
+++ process_src_coverage.jl
@@ -0,0 +1,24 @@
+using Coverage
+
+function process_folder2(folder="src")
+    @info "Coverage.process_folder2: Searching $folder for .jl files..."
+    source_files = FileCoverage[]
+    files = readdir(folder)
+    for file in files
+        fullfile = joinpath(folder, file)
+        if isfile(fullfile)
+            # Is it a Julia file?
+            if splitext(fullfile)[2] == ".jl"
+                push!(source_files, process_file(fullfile, folder))
+            else
+                @debug "Coverage.process_folder2: Skipping $file, not a .jl file"
+            end
+        elseif isdir(fullfile) && (file != "legacy")
+            # If it is a folder, recursively traverse
+            append!(source_files, process_folder2(fullfile))
+        else
+            @debug "Coverage.process_folder2: Skipping folder $file"
+        end
+    end
+    return source_files
+end
